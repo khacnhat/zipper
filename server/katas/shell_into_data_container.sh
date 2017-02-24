0e9a6c278cfb2a751cdd87bd0cb4e591f6c7e45d
Jon Jagger <jon@jaggersoft.com>
2017-02-24 09:47:56 +0000

    refactoring; use .env in script to avoid duplication

--- server/katas/shell_into_data_container.sh
+++ server/katas/shell_into_data_container.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
-CONTAINER_NAME=cyber-dojo-zipper-kata-DATA-CONTAINER
+MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-docker run --rm -it --volumes-from ${CONTAINER_NAME} --workdir /tmp/katas cyberdojo/ruby sh
+. ${MY_DIR}/../../.env
+
+docker run --rm -it \
+  --volumes-from ${CYBER_DOJO_ZIPPER_KATA_DATA_CONTAINER} \
+  --workdir ${CYBER_DOJO_KATAS_ROOT} \
+  cyberdojo/ruby sh
