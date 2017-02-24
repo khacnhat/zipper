1e2cbdabf53f27db4473e575e685d798b3fe53a3
Jon Jagger <jon@jaggersoft.com>
2017-02-24 09:41:35 +0000

    refactoring; use .env in scripts to avoid duplication

--- server/katas/create_data_container.sh
+++ server/katas/create_data_container.sh
@@ -4,9 +4,8 @@ set -e
 # called from pipe_build_up_test.sh
 
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-CONTEXT_DIR=${MY_DIR}
-TAG=cyberdojo/zipper_kata
-CYBER_DOJO_ZIPPER_KATA_DATA_CONTAINER=cyber-dojo-zipper-kata-DATA-CONTAINER
+
+. ${MY_DIR}/../../.env
 
 name="${CYBER_DOJO_ZIPPER_KATA_DATA_CONTAINER}"
 if [ "$(docker ps --all --quiet --filter name=${name})" != "" ]; then
@@ -14,9 +13,11 @@ if [ "$(docker ps --all --quiet --filter name=${name})" != "" ]; then
 fi
 
 cd ${MY_DIR}
+CONTEXT_DIR=${MY_DIR}
+TAG=cyberdojo/zipper_kata
 
 docker build \
-  --build-arg=CYBER_DOJO_ZIPPER_KATA_ROOT=/tmp/katas \
+  --build-arg=CYBER_DOJO_KATAS_ROOT=${CYBER_DOJO_KATAS_ROOT} \
   --tag=${TAG} \
   --file=Dockerfile \
   ${CONTEXT_DIR}
