58b436da5adc095daddf32c7ef65efa2ededec80
Jon Jagger <jon@jaggersoft.com>
2017-02-24 12:05:08 +0000

    use env-vars from .env file in docker-compose.yml

--- up.sh
+++ up.sh
@@ -2,5 +2,5 @@
 set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-docker-compose --file ${my_dir}/docker-compose.yml down
+docker-compose --file ${my_dir}/docker-compose.yml down -v
 docker-compose --file ${my_dir}/docker-compose.yml up -d
