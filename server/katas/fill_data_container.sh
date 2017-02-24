2a5024de5ebae4a63ccee2a4e747e70531fb6cc4
Jon Jagger <jon@jaggersoft.com>
2017-02-24 09:32:13 +0000

    refactoring; use .env in scripts to avoid duplication

--- server/katas/fill_data_container.sh
+++ server/katas/fill_data_container.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
+
 # called from pipe_build_up_test.sh
 
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-CYBER_DOJO_ZIPPER_KATA_ROOT=/tmp/katas
-CYBER_DOJO_ZIPPER_KATA_DATA_CONTAINER=cyber-dojo-zipper-kata-DATA-CONTAINER
+. ${MY_DIR}/../../.env
 
 cd ${MY_DIR}
 
@@ -14,7 +14,7 @@ for ID in DA F6 1D 69 7A
 do
   docker cp \
     ${MY_DIR}/${ID} \
-    ${CYBER_DOJO_ZIPPER_KATA_DATA_CONTAINER}:${CYBER_DOJO_ZIPPER_KATA_ROOT}/${ID}
+    ${CYBER_DOJO_ZIPPER_KATA_DATA_CONTAINER}:${CYBER_DOJO_KATAS_ROOT}/${ID}
 done
 
 # can't exec directly into zipper-kata-data-container as it is not running
