82653fcd154dea2b21f005e5d5ecb84fb03d8cf5
Jon Jagger <jon@jaggersoft.com>
2017-03-07 10:22:25 +0000

    refactoring

--- client/test/run.sh
+++ client/test/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# This is the only shell script that needs bash rather than sh
-# It needs it for the (array) handling below.
-# Called from test.sh (from outside the docker-container)
-
 if [ ! -f /.dockerenv ]; then
   echo 'FAILED: run.sh is being executed outside of docker-container.'
-  echo 'Use test.sh which first calls build.sh'
+  echo 'Use pipe_build_up_test.sh'
   exit 1
 fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# client
 
 cov_dir=${CYBER_DOJO_COVERAGE_ROOT}
 test_log=${cov_dir}/test.log
@@ -17,5 +16,9 @@ my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 cd ${my_dir}/src
 files=(*_test.rb)
 args=(${*})
-ruby -e "([ '../coverage.rb' ] + %w(${files[*]}).shuffle).each{ |file| require './'+file }" -- ${args[@]} | tee ${test_log}
-cd ${my_dir} && ruby ./check_test_results.rb ${test_log} ${cov_dir}/index.html > ${cov_dir}/done.txt
+
+ruby -e "([ '../coverage.rb' ] + %w(${files[*]}).shuffle).each{ |file| require './'+file }" \
+  -- ${args[@]} | tee ${test_log}
+
+cd ${my_dir} \
+  && ruby ./check_test_results.rb ${test_log} ${cov_dir}/index.html > ${cov_dir}/done.txt
