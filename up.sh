de6e0479888c0161543a028c8c0a95825b4b976d
Jon Jagger <jon@jaggersoft.com>
2017-03-15 08:14:20 +0000

    added checks to exit up.sh if client/server containers exit on startup

--- up.sh
+++ up.sh
@@ -2,5 +2,24 @@
 set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-docker-compose --file ${my_dir}/docker-compose.yml down -v
+docker-compose --file ${my_dir}/docker-compose.yml down --volumes
 docker-compose --file ${my_dir}/docker-compose.yml up -d
+
+sleep 1
+
+check_up()
+{
+  set +e
+  local s=$(docker ps --filter status=running --format '{{.Names}}' | grep ^${1}$)
+  set -e
+  if [ "${s}" != "${1}" ]; then
+    echo
+    echo "${1} exited"
+    docker logs ${1}
+    exit 1
+  fi
+}
+
+check_up 'zipper_server'
+check_up 'zipper_client'
+check_up 'storer_server'
