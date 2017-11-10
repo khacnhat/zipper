fad8efa1b71c3dbda348335fb188af09829430ff
Jon Jagger <jon@jaggersoft.com>
2017-11-10 09:05:37 +0000

    switched client from sinatra to rack; now same as server so similar Dockerfiles make for quicker build

--- client/src/demo.rb
+++ client/src/demo.rb
@@ -1,19 +1,21 @@
 require_relative 'zipper_service'
-require 'sinatra'
-require 'sinatra/base'
 
-class Demo < Sinatra::Base
+class Demo
 
-  get '/zip' do
+  def call(_env)
     tgz_filename = zipper.zip('7AF23949B7')
 
     colour = 'white'
     border = 'border:1px solid black'
     padding = 'padding:10px'
     background = "background:#{colour}"
-    "<pre style='#{border};#{padding};#{background}'>" +
-    tgz_filename +
-    '</pre>'
+    html = ''
+
+    html += "<pre style='#{border};#{padding};#{background}'>"
+    html += tgz_filename
+    html += '</pre>'
+
+    [ 200, { 'Content-Type' => 'text/html' }, [ html ] ]
   end
 
   private
