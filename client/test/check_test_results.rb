9fda57317a6c32f0b41bd509b613cf61d0f58c1b
Jon Jagger <jon@jaggersoft.com>
2017-11-10 07:37:41 +0000

    bump test metrics

--- client/test/check_test_results.rb
+++ client/test/check_test_results.rb
@@ -97,7 +97,7 @@ table =
     [ 'failures',               failure_count,      '==',   0 ],
     [ 'errors',                 error_count,        '==',   0 ],
     [ 'skips',                  skip_count,         '==',   0 ],
-    [ 'assertions/s',           assertions_per_sec, '>=',  30 ],
+    [ 'assertions/s',           assertions_per_sec, '>=',  25 ],
     [ 'duration(test)[s]',      test_duration,      '<=',   5 ],
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -98,10 +98,10 @@ table =
     [ 'errors',                 error_count,        '==',   0 ],
     [ 'skips',                  skip_count,         '==',   0 ],
     [ 'assertions/s',           assertions_per_sec, '>=', 100 ],
-    [ 'duration(test)[s]',      test_duration,      '<=',   3 ],
+    [ 'duration(test)[s]',      test_duration,      '<=',   4 ],
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
-    [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 200 ],
+    [ 'hits_per_line(src)',     hits_per_line_src,  '<=', 160 ],
     [ 'hits_per_line(test)',    hits_per_line_test, '<=',  15 ],
     [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 1.2 ],
   ]
