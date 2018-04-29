e60f0a7117484948884c0b8e364e05afe0af12cc
Jon Jagger <jon@jaggersoft.com>
2018-04-29 16:44:31 +0100

    refactoring; bash quote style

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -9,7 +9,10 @@ set -e
 
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
-${SH_DIR}/build_docker_images.sh
-${SH_DIR}/docker_containers_up.sh
-${SH_DIR}/run_tests_in_containers.sh ${*}
-${SH_DIR}/docker_containers_down.sh
+export SHA=$(cd "${SH_DIR}" && git rev-parse HEAD)
+
+"${SH_DIR}/build_docker_images.sh"
+"${SH_DIR}/docker_containers_up.sh"
+if "${SH_DIR}/run_tests_in_containers.sh" "$@"; then
+  "${SH_DIR}/docker_containers_down.sh"
+fi
--- sh/run_demo.sh
+++ sh/run_demo.sh
@@ -2,7 +2,7 @@
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
-${MY_DIR}/build_docker_images.sh
-${MY_DIR}/docker_containers_up.sh
+"${MY_DIR}/build_docker_images.sh"
+"${MY_DIR}/docker_containers_up.sh"
 
 echo 'port=4588'
